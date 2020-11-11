f073cd739b0aeb516f20c17c80df05dd58622859
Buwaneka Boralessa <buwanekab@gmail.com>
2020-11-11 20:10:14 +0530

    added process `uncaughtException` handler

--- src/server.ts
+++ src/server.ts
@@ -9,3 +9,8 @@ app.get('/', (req, res) => res.send('its working'));
 app.listen(port, () => {
   Logger.info(`Server is running at localhost:${port}`);
 });
+
+process.on('uncaughtException', (err) => {
+  Logger.error('uncaughtException : ', err.message, err.stack);
+  process.exit(1);
+});
