a11e59a0cfc971d9fc6e7d180f0132f7a71a4278
ctennakoon <ctennakoon@knewton.com>
2020-11-20 00:42:23 +0530

    Setup static code analyzer tools for programming language

--- src/constant/common-constants.ts
+++ src/constant/common-constants.ts
@@ -15,7 +15,4 @@ export const DOCKER_IMAGE_TAGS = {
   [Type.JAVA]: 'openjdk:14-jdk-alpine',
   [Type.NODE]: 'mhart/alpine-node:14',
   [Type.PYTHON]: 'python:3.9.0-alpine3.12',
-  [CustomType.CUSTOM_JAVA]: `${CUSTOM_DOCKER_IMAGES[CustomType.CUSTOM_JAVA].tag}:latest`,
-  [CustomType.CUSTOM_NODE]: `${CUSTOM_DOCKER_IMAGES[CustomType.CUSTOM_NODE].tag}:latest`,
-  [CustomType.CUSTOM_PYTHON]: `${CUSTOM_DOCKER_IMAGES[CustomType.CUSTOM_PYTHON].tag}:latest`,
 };
